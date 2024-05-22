51294f8f4936f3c88ff9b09a61550efccfd4bbb9
UmairIshaq008 <163378587+UmairIshaq008@users.noreply.github.com>
2024-05-22 23:38:36 +0500

    Add files via upload

---
+++ Ajax/index.js
@@ -0,0 +1,10 @@
+
+
+$(function () {
+    $('#load').click(function () {
+      $.get('students.txt', function (Response) {
+        $('#result').empty()
+        $('#result').append(Response)
+      })
+    })
+  })
